be38f29ebb6eefda7550c84431ba2c2434bb099e
Hugo <hugo.sepulvedao@usm.cl>
2024-11-06 19:14:59 -0300

    connect search trucks result page to endpoints

--- src/app/(mineguard)/searchTruck/page.tsx
+++ src/app/(mineguard)/searchTruck/page.tsx
@@ -9,6 +9,20 @@ import TrucksTable from 'components/search/TrucksTable';
 
 const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
 
+const requestBodyGetTrucks = {
+  query: `
+    query GetTrucksResults {
+      getTrucksResults {
+        serie
+        model
+        driver
+        driverID
+        status
+      }
+    }
+  `
+};
+
 const SearchTruckPage = () => {
   const searchParams = useSearchParams();
   const truck = searchParams.get("truck");
@@ -17,31 +31,46 @@ const SearchTruckPage = () => {
 
   const [trucksData, setTrucksData] = useState<any[]>([]);
 
-  const exampleData = [
-    {
-      truck: "model 1",
-      serie: "A50001",
-      driver: "Brandon Monsalve",
-      state: "Operativo"
-    },
-    {
-      truck: "model 2",
-      serie: "A50002",
-      driver: "Nicolás Vargas",
-      state: "Inactivo"
-    },
-    {
-      truck: "model 3",
-      serie: "A50003",
-      driver: "Claudio Inal",
-      state: "Operativo"
-    }
-  ]
+  // Fetch GetTrucksResults
+  useEffect(() => {
+
+    fetch(ENDPOINT, {
+      method: "POST",
+      body: JSON.stringify(requestBodyGetTrucks),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.errors) {
+          console.error("Error:", data.errors[0].message);
+        }
+        if (data && data.data && data.data.getTrucksResults) {
+          console.log(data.data.getTrucksResults);
+          setTrucksData(data.data.getTrucksResults);
+          
+          if (truck) {
+            setTrucksData(prevData => {
+              return prevData.filter(truck_ => truck_.model.toLowerCase().includes(truck.toLowerCase()));
+            })
+          }
+          if (serie) {
+            setTrucksData(prevData => {
+              return prevData.filter(truck => truck.serie.toLowerCase().includes(serie.toLowerCase()));
+            })
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching trucks information:", error);
+      });
+  }, []);
 
   return (
     <Flex direction="column" pt={{ sm: '125px', lg: '75px' }}>
       <Card px="0px">
-        <TrucksTable tableData={exampleData} />
+        <TrucksTable tableData={trucksData} />
       </Card>
     </Flex>
   );
--- src/components/search/TrucksTable.tsx
+++ src/components/search/TrucksTable.tsx
@@ -51,15 +51,34 @@ import {
 } from '@tanstack/react-table';
 import { MdDelete, MdEdit } from "react-icons/md";
 
+const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
+
+const requestBodyGetDrivers = {
+    query: `
+        query GetDrivers {
+            getDrivers {
+                _id
+                name
+                contactNumber
+                userType
+            }
+        }
+    `
+};
+
 type RowObj = {
   serie: string;
-  truck: string;
+  model: string;
   driver: string;
-  state: string;
+  status: string;
+  driverID: string;
 };
 
 export default function AlertsTable(props: { tableData: RowObj[] }) {
     const { tableData } = props;
+    const uniqueModels = [... new Set(tableData.map(truck => truck.model))];
+    const [driversData, setDriversData] = React.useState([]);
+    const [uniqueDriverNames, setUniqueDriverNames] = React.useState([]);
     const { isOpen, onOpen, onClose } = useDisclosure();
     const cancelRef = useRef();
     const textColor = useColorModeValue('navy.700', 'white');
@@ -78,6 +97,32 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
         setData(tableData);
     }, [tableData]);
 
+    // Fetch GetDrivers
+    useEffect(() => {
+
+    fetch(ENDPOINT, {
+        method: "POST",
+        body: JSON.stringify(requestBodyGetDrivers),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.errors) {
+                console.error("Error:", data.errors[0].message);
+            }
+            if (data && data.data && data.data.getDrivers) {
+                console.log(data.data.getDrivers);
+                setDriversData(data.data.getDrivers);
+                setUniqueDriverNames([... new Set(data.data.getDrivers.map(driver => driver.name))])
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching drivers information:", error);
+        });
+    }, []);
+
     const handleEdit = (index: number) => {
         setEditRowIndex(index);
         editRowDataRef.current = { ...data[index] }; // Copia los datos actuales de la fila
@@ -85,21 +130,63 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
 
     const handleAccept = () => {
         if (!editRowDataRef.current.driver || !editRowDataRef.current.serie || 
-            !editRowDataRef.current.state || !editRowDataRef.current.truck) {
+            !editRowDataRef.current.status || !editRowDataRef.current.model) {
             setAlertMessage('Por favor, rellena todos los campos para editar un registro.');
             return;
         } else {
             setAlertMessage('');
         }
         if (editRowIndex !== null && editRowDataRef.current) {
-            const updatedData = [...data];
-            updatedData[editRowIndex] = editRowDataRef.current;
-            setData(updatedData);
 
-            console.log(editRowDataRef.current);
+            const driverID = driversData.find(driver => driver.name === editRowDataRef.current.driver)._id;
 
-            setEditRowIndex(null);
-            editRowDataRef.current = null;
+            const requestBodyUpdateDriver = {
+                query: `
+                    mutation UpdateTruck($truck: TruckUpdate) {
+                        updateTruck(truck: $truck)
+                    }
+                `,
+                variables: {
+                    "truck": {
+                        "driverID": driverID,
+                        "model": editRowDataRef.current.model,
+                        "serie": editRowDataRef.current.serie,
+                        "status": editRowDataRef.current.status
+                    }
+                }
+            };
+            
+            // fetch UpdateTruck
+            fetch(ENDPOINT, {
+                method: "POST",
+                body: JSON.stringify(requestBodyUpdateDriver),
+                headers: {
+                "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.errors) {
+                        console.error("Error:", data.errors[0].message);
+                    }
+                    if (data && data.data && data.data.updateTruck) {
+                        console.log("Camión actualizado exitosamente:", data.data.updateTruck);
+                        
+                        setData((prevData) => {
+                            const updatedData = [...prevData];
+                            updatedData[editRowIndex] = editRowDataRef.current;
+                            return updatedData;
+                        });
+
+                        console.log(editRowDataRef.current);
+            
+                        setEditRowIndex(null);
+                        editRowDataRef.current = null;
+                    }
+                })
+                .catch((error) => {
+                    console.log("Error:", error);
+                });
         }
     };
 
@@ -122,13 +209,48 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
 
     const confirmDelete = () => {
         if (idToDelete !== null) {
-            console.log(data[idToDelete]);
-            const updatedData = data.filter((_, index) => index !== idToDelete);
-            setData(updatedData);
-            setIdToDelete(null);
+
+            const requestBodyDeleteDriver = {
+                query: `
+                    mutation DeleteTruck($serie: ID) {
+                        deleteTruck(serie: $serie)
+                    }
+                `,
+                variables: {
+                    "serie": data[idToDelete]?.serie
+                }
+            };
+
+            fetch(ENDPOINT, {
+                method: "POST",
+                body: JSON.stringify(requestBodyDeleteDriver),
+                headers: {
+                "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.errors) {
+                        console.error("Error:", data.errors[0].message);
+                    }
+                    if (data && data.data && data.data.deleteTruck) {
+                        console.log("Camión eliminado exitosamente:", data.data.deleteTruck);
+                        
+                        setData((prevData) => {
+                            console.log(prevData[idToDelete]);
+                            const updatedData = prevData.filter((_, index) => index !== idToDelete);
+                            return updatedData;
+                        });
+
+                        onClose();
+                        setIdToDelete(null);
+                    }
+                })
+                .catch((error) => {
+                    console.log("Error:", error);
+                });
+            }
         }
-        onClose();
-    }
 
     const [columnFilters, setColumnFilters] =
         React.useState<ColumnFiltersState>([]);
@@ -136,8 +258,8 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
     const [globalFilter, setGlobalFilter] = React.useState('');
     const columnHelper = createColumnHelper<RowObj>();
     const columns = [
-        columnHelper.accessor('truck', {
-        id: 'truck',
+        columnHelper.accessor('model', {
+        id: 'model',
         header: () => (
             <Text fontSize={{ sm: '10px', lg: '12px' }} color="gray.400">
             Camión
@@ -147,11 +269,12 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
             const rowIndex = info.row.index;
             return rowIndex === editRowIndex ? (
             <Select
-                defaultValue={editRowDataRef.current?.truck || ''}
-                onChange={(e) => handleInputChange('truck', e.target.value)}
+                defaultValue={editRowDataRef.current?.model || ''}
+                onChange={(e) => handleInputChange('model', e.target.value)}
             >
-                <option value="Truck1">Truck1</option>
-                <option value="Truck2">Truck2</option>
+                {uniqueModels.map((model, index) => (
+                    <option key={`${model}${index}`} value={model}>{model}</option>
+                ))}
             </Select>
             ) : (
             <Text color={textColor} fontSize="md" fontWeight="500">
@@ -191,10 +314,14 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
         cell: (info) => {
             const rowIndex = info.row.index;
             return rowIndex === editRowIndex ? (
-            <Input
+            <Select
                 defaultValue={editRowDataRef.current?.driver || ''}
                 onChange={(e) => handleInputChange('driver', e.target.value)}
-            />
+            >
+                {uniqueDriverNames.map((driverNmae, index) => (
+                    <option key={`${driverNmae}${index}`} value={driverNmae}>{driverNmae}</option>
+                ))}
+            </Select>
             ) : (
             <Text color={textColor} fontSize="md" fontWeight="500">
                 {info.getValue()}
@@ -202,8 +329,8 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
             );
         },
         }),
-        columnHelper.accessor('state', {
-        id: 'state',
+        columnHelper.accessor('status', {
+        id: 'status',
         header: () => (
             <Text fontSize={{ sm: '10px', lg: '12px' }} color="gray.400">
             Estado
@@ -213,15 +340,15 @@ export default function AlertsTable(props: { tableData: RowObj[] }) {
             const rowIndex = info.row.index;
             return rowIndex === editRowIndex ? (
             <Select
-                defaultValue={editRowDataRef.current?.state || ''}
-                onChange={(e) => handleInputChange('state', e.target.value)}
+                defaultValue={editRowDataRef.current?.status || ''}
+                onChange={(e) => handleInputChange('status', e.target.value)}
             >
-                <option value="Operativo">Operativo</option>
-                <option value="Inactivo">Inactivo</option>
+                <option value="operative">Operativo</option>
+                <option value="idle">Inactivo</option>
             </Select>
             ) : (
             <Badge
-                colorScheme={info.getValue() === 'Inactivo' ? 'red' : 'green'}
+                colorScheme={info.getValue() === 'idle' ? 'red' : 'green'}
                 fontSize="md"
                 fontWeight="500"
                 >
